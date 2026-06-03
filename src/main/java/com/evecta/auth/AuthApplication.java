cdce87676663c56c82c03732a56fb25ff6799d6a
Andrés Ortega <andythe20xd@gmail.com>
2026-06-03 13:26:53 -0400

    se implementa logica para enviar objeto audit a core

--- src/main/java/com/evecta/auth/AuthApplication.java
+++ src/main/java/com/evecta/auth/AuthApplication.java
@@ -2,12 +2,15 @@ package com.evecta.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableAsync
 public class AuthApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AuthApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(AuthApplication.class, args);
+  }
 }
